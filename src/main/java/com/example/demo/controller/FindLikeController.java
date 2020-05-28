d85402b941251c3a1aacc3050bdaf9ca103d8aba
ymt135799 <690486631@qq.com>
2020-05-28 09:16:42 +0800

    1

--- src/main/java/com/example/demo/controller/FindLikeController.java
+++ src/main/java/com/example/demo/controller/FindLikeController.java
@@ -9,8 +9,11 @@ import com.example.demo.entity.TUser;
 import com.example.demo.service.TArticleService;
 import com.example.demo.service.TCorpusService;
 import com.example.demo.service.TUserService;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -33,17 +36,21 @@ public class FindLikeController {
     private TCorpusService corpusService;
 
     @GetMapping("findLike")
-    public ApiResult findLike(FindDTO find) {
+    public ApiResult findLike(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "6") Integer size, FindDTO find) {
+        PageHelper.startPage(page, size);
         Integer type = find.getType();
         if (type == 0) {
             List<TUser> userList = userService.findLike(find);
-            return ApiResult.resultWith(ResultCodeEnum.SUCCESS, userList);
+            PageInfo<TUser> pageInfo = new PageInfo<>(userList);
+            return ApiResult.resultWith(ResultCodeEnum.SUCCESS, pageInfo);
         } else if (type == 1) {
             List<TArticle> articleList = articleService.findLike(find);
-            return ApiResult.resultWith(ResultCodeEnum.SUCCESS, articleList);
+            PageInfo<TArticle> pageInfo = new PageInfo<>(articleList);
+            return ApiResult.resultWith(ResultCodeEnum.SUCCESS, pageInfo);
         } else {
             List<TCorpus> corpusList = corpusService.findLike(find);
-            return ApiResult.resultWith(ResultCodeEnum.SUCCESS, corpusList);
+            PageInfo<TCorpus> pageInfo = new PageInfo<>(corpusList);
+            return ApiResult.resultWith(ResultCodeEnum.SUCCESS, pageInfo);
         }
     }
 }
